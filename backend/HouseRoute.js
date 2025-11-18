7515b132423dfbf6b29feed67517e45cd5685a42
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-18 12:12:53 +0200

    backend: implementing house selling app

--- backend/HouseRoute.js
+++ backend/HouseRoute.js
@@ -7,9 +7,21 @@ route.post('/add', async(req, res) => {
     const { title, description, price, location, bathrooms, size, yearBuilt, parkingSpace, hasGarden, PropertyType } = req.body;
 
     try {
+        if (!req.session.userInfo) {
+            return res.status(401).json({message: 'Login first' });
+        }
+
+        const owner = req.session.userInfo._id;
         if (!title || !description || !price || !location || !bathrooms || !size || yearBuilt) {
+
             await HouseSchema.create({
-                
+                 title,
+                 description,
+                 price,
+                 location,
+                 bathrooms,
+                 size,
+                 yearBuilt
             })
         }
     }
