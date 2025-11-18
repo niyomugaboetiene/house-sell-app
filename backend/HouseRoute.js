9e14ad7a4d49dc5ce29d31ea34621d8b706144f2
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-18 12:10:33 +0200

    backend: implementing house selling app

--- backend/HouseRoute.js
+++ backend/HouseRoute.js
@@ -5,4 +5,12 @@ const route = express.Router();
 
 route.post('/add', async(req, res) => {
     const { title, description, price, location, bathrooms, size, yearBuilt, parkingSpace, hasGarden, PropertyType } = req.body;
+
+    try {
+        if (!title || !description || !price || !location || !bathrooms || !size || yearBuilt) {
+            await HouseSchema.create({
+                
+            })
+        }
+    }
 })
