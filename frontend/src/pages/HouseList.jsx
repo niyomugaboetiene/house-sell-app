e9489ef5a2b7b4b3d9c4934f79532d4f6514874c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-19 11:31:09 +0200

    frontend: implementing house selling app

--- frontend/src/pages/HouseList.jsx
+++ frontend/src/pages/HouseList.jsx
@@ -23,7 +23,38 @@ const HouseListComponent = () => {
                 <h1>House list</h1>
                 {house.map((item, idx) => (
                     <div key={idx}>
+                        {/*            title,
+                 description,
+                 price,
+                 location: locationData,
+                 bathrooms,
+                 size,
+                 yearBuilt,
+                 parkingSpace,
+                 hasGarden,
+                 PropertyType,
+                 owner: owner,
+                 isAvailable,
+                 image: imagePath,
+                 video: videoPath */}
                         <p>{item.title}</p>
+                        <p>{item.description}</p>
+                        <p>{item.price}</p>
+                        {item.location && (
+                            `Location: ${item.location.country}, 
+                             City: ${item.location.city},
+                             District: ${item.location.district}, 
+                             Sector: ${item.location.sector}, 
+                             Street: ${item.location.street}`
+                        )}
+                        <p>Bathrooms: {item.bathrooms}</p>
+                        <p>Size: {item.size}</p>
+                        <p>Year Built: {item.size}</p>
+                        <p>Has Garden: {item.hasGarden ? "Yes": "No"}</p>
+                        <p>Property Type: {item.PropertyType}</p>
+                        <p>Available: {item.isAvailable ? "Yes": "No" }</p>
+                        <img src={`http://localhost:5000/House_Images/${item.image }`} alt="" />
+                        <video src={`http://localhost:5000/House_Images/${item.video}`}></video>
                     </div>
                 ))}
             </div>
