dc922a8a8694185f8482f84a035b06999840983d
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-19 11:15:02 +0200

    frontend: implementing house selling app

---
+++ frontend/src/pages/HouseList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const HouseListComponent = () => {
+    const [house, setHouse] = useState([]);
+    
+    const HouseList = async() => {
+        const res = await axios.get('http://localhost:5000/house/houses', { withCredentials: true });
+        setHouse(res);
+    }
+
+    useEffect(() => {
+        HouseList();
+    }, []);
+
+
+    return (
+        <div>
+            <div>
+                <h1>House list</h1>
+                {house.map((idx, item) => (
+                    <div key={idx}>
+                        <p>{item.title}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default HouseListComponent;
