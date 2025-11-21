87dcf0e1de5770af426d498181b7a74286d20c57
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-21 10:47:00 +0200

    house_sell: implementing about Houses list

--- backend/UserSchema.js
+++ backend/UserSchema.js
@@ -26,11 +26,9 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now()
     },
-    image: [
-        {
+    image: {
             type: String
-        }
-    ]
+    }
 
 })
 
